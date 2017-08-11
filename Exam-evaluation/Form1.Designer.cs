6e104a6d97ff83cecfea26501b974ab7503aa67c
SiamRafsunjani <siam_rafsan@yahoo.com>
2017-08-12 03:02:59 +0600

    MainWindow added

--- Exam-evaluation/Form1.Designer.cs
+++ Exam-evaluation/Form1.Designer.cs
@@ -28,12 +28,31 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.MainWindow = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // MainWindow
+            // 
+            this.MainWindow.Location = new System.Drawing.Point(1, 0);
+            this.MainWindow.Name = "MainWindow";
+            this.MainWindow.Size = new System.Drawing.Size(780, 431);
+            this.MainWindow.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 432);
+            this.Controls.Add(this.MainWindow);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel MainWindow;
     }
 }
 
